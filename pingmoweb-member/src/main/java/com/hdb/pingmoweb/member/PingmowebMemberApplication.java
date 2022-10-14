4edba5f831bdd8eac5adef9cc97aee20c9a025d3
hdb <874171934@qq.com>
2022-10-14 14:53:42 +0800

    nacos 配置

--- pingmoweb-member/src/main/java/com/hdb/pingmoweb/member/PingmowebMemberApplication.java
+++ pingmoweb-member/src/main/java/com/hdb/pingmoweb/member/PingmowebMemberApplication.java
@@ -2,7 +2,9 @@ package com.hdb.pingmoweb.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages="com.hdb.pingmoweb.member.feign")//扫描接口方法注解
 @SpringBootApplication
 public class PingmowebMemberApplication {
 
--- pingmoweb-product/src/test/java/com/hdb/pingmoweb/product/PingmowebProductApplicationTests.java
+++ pingmoweb-product/src/test/java/com/hdb/pingmoweb/product/PingmowebProductApplicationTests.java
@@ -1,13 +1,29 @@
 package com.hdb.pingmoweb.product;
 
+import com.hdb.pingmoweb.product.entity.BrandEntity;
+import com.hdb.pingmoweb.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PingmowebProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("1");
+        brandEntity.setLogo("1");
+        brandEntity.setSort(2);
+        brandEntity.setFirstLetter("1");
+        brandEntity.setShowStatus(1);
+        brandService.save(brandEntity);
     }
 
 }
